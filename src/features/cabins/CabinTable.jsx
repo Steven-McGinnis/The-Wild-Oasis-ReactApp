cea0a0400f7809b366a678e906089cbb3c5477b7
Steven McGinnis <steven.mcginnis@itmmarketing.com>
2025-02-08 12:58:39 -0500

    Add cabin filtering and sorting functionality with new UI components

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,11 +4,27 @@ import Spinner from '../../ui/Spinner';
 import CabinRow from './CabinRow';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
+import { useSearchParams } from 'react-router-dom';
 
 const CabinTable = () => {
   const { isPending, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
   if (isPending) return <Spinner />;
 
+  // 1) FILTER
+  const filterValue = searchParams.get('discount') || 'all';
+  console.log(filterValue);
+
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0);
+  if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-')
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a, b) => (a[field] - b[field]) * modifier );
   return (
     <Menus>
       <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
@@ -21,7 +37,7 @@ const CabinTable = () => {
           <div className=''></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => (
             <CabinRow
               cabin={cabin}
