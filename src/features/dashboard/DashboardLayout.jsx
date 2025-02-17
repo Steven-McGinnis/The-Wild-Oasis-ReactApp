7fe823f04a7bf83072c5eec00b007cfb12ea9a00
Steven McGinnis <steven.mcginnis@itmmarketing.com>
2025-02-16 21:02:19 -0500

    Add recent bookings and stays hooks with loading state and integrate into Dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,24 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { bookings, isPending: isPendingBookings } = useRecentBookings();
+  const { stays, isPending: isPendingStays, confirmedStays } = useRecentStays();
+
+  if (isPendingBookings || isPendingStays) return <Spinner />;
+
+  console.log(bookings);
+  console.log(stays);
+  console.log(confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div className=''>Statistics</div>
+      <div className=''>Todays Activity</div>
+      <div className=''>Chart Stay Duration</div>
+      <div className=''>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+};
+export default DashboardLayout;
